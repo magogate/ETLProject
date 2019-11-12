ef354120e658d4688020b9ebffef9ae175ed1702
mandargogate <mandargogate@outlook.com>
2019-11-11 20:22:04 -0500

    added header/comments

--- fetchGlassdoorJobs.py
+++ fetchGlassdoorJobs.py
@@ -1,3 +1,17 @@
+# Created By: 
+#     Grettel Canepari
+#     Katherine Lee
+#     Mandar Gogate
+#     Petra Alex
+#     Preet Puri
+#     Sweta Shekhar
+# Created On: 11/09/2019
+# This program performs following steps in order to pull job information from glassdoor jobsite
+# 1. search glassdoor jobs with city/state name on google first
+# 2. Extract very first glassdoor url from google search 
+# 3. Using glassdoor url, it derives 3 URLs for first 3 pages of glassdoor for respective city/state 
+# 4. Extracts Employer Name, Job Title, Salary and Job Posting Days for first 3 pages using derived URL at Step#3
+
 import requests
 import bs4
 import re
